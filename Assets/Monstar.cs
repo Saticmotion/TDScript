f54b4df6fe9ab59eb29890b9904b5185c08bb7f3
Saticmotion <simon_verbeke_@hotmail.com>
2017-12-16 02:41:34 +0100

    Initial commit

---
+++ Assets/Monstar.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Monstar : MonoBehaviour
+{
+	public GameObject prevPoint;
+	public GameObject nextPoint;	
+	public int index;
+	public bool dead;
+
+	void Start ()
+	{
+		prevPoint = Path.instance.points[0];
+		nextPoint = Path.instance.points[1];
+	}
+	
+	void Update () 
+	{
+		if ((transform.position - nextPoint.transform.position).magnitude < 0.05f)
+		{
+			if (index < Path.instance.points.Count - 1)
+			{
+				index++;
+				prevPoint = nextPoint;
+				nextPoint = Path.instance.points[index];
+			}
+			else
+			{
+				dead = true;
+			}
+		}
+
+		var direction = Vector3.Normalize(nextPoint.transform.position - prevPoint.transform.position);
+		transform.position += direction * 4f * Time.deltaTime;
+	}
+}
---
+++ Assets/Path.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+	public static Path instance
+	{
+		get { return _instance; }
+		set { _instance = value; }
+	}
+	private static Path _instance;
+
+	public List<GameObject> points;
+
+	public GameObject monstarPrefab;
+	public List<GameObject> monstars;
+
+	public float spawnInterval = 2;
+	public float timeSinceLastSpawn;
+
+	void Start ()
+	{
+		_instance = this;
+	}
+
+	void Update()
+	{
+		timeSinceLastSpawn += Time.deltaTime;
+		if (timeSinceLastSpawn > spawnInterval)
+		{
+			var monstar = Instantiate(monstarPrefab, points[0].transform.position, Quaternion.identity);
+			monstars.Add(monstar);
+			timeSinceLastSpawn = 0;
+		}
+
+		for (int i = monstars.Count - 1; i >= 0; i--)
+		{
+			if (monstars[i].GetComponent<Monstar>().dead)
+			{
+				Destroy(monstars[i]);
+				monstars.RemoveAt(i);
+			}
+		}
+	}
+}
